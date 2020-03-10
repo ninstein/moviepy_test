b1b490e6f68880a8c9d67b34bd2bd14ab541ab68
ninstein <ninstein@gmail.com>
2020-03-10 14:09:30 +0800

    subclip

--- videotest.py
+++ videotest.py
@@ -1,7 +1,7 @@
 from moviepy.editor import VideoFileClip, concatenate_videoclips
  
-clip1 = VideoFileClip("v2.mp4")
-clip2 = VideoFileClip("v3.mp4")
+clip1 = VideoFileClip("v2.mp4").subclip(1,2)
+clip2 = VideoFileClip("v3.mp4").subclip(1,2)
 #clip3 = VideoFileClip("v3.mp4")
  
 finalclip = concatenate_videoclips([clip1, clip2])
